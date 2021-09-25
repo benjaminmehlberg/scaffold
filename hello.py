b85fbac52b7c213f91fbcb71959fd99910a10905
benjaminmehlberg <benjamin.mehlberg@gmail.com>
2021-09-25 14:01:19 +0200

    Debugging

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
-
+"""
+A test script
+"""
 def add(x, y):
     return x + y
 
 result = add(1, 2)
 print("The sum of 1 and 2 is {}".format(result))
-'''
 
+'''
 def toyou(x):
     return f"hi {x}"
 
